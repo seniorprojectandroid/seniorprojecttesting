535e5b6a0ffe0bc2c014042c0e8688e689ed53f9
Julio <jreye087@fiu.edu>
2012-09-26 21:30:28 -0400

    Implement main

--- seniorprojecttesting/src/edu/fiu/cs/seniorproject/test/MainActivityTest.java
+++ seniorprojecttesting/src/edu/fiu/cs/seniorproject/test/MainActivityTest.java
@@ -1,11 +1,14 @@
 package edu.fiu.cs.seniorproject.test;
 
 import edu.fiu.cs.seniorproject.MainActivity;
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
 	private MainActivity mActivity = null;
+	private Button mButton = null;
 	
 	public MainActivityTest(Class<MainActivity> activityClass) {
 		super(activityClass);
@@ -17,11 +20,43 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 	
 	@Override
 	protected void setUp() {
+		setActivityInitialTouchMode(false);
 		mActivity = getActivity();
-		
+		mButton = (Button)mActivity.findViewById(edu.fiu.cs.seniorproject.R.id.events_button);
 	}
 	
-	public void testActivity() {
+	public void testPreconditions() {
 		assertNotNull(mActivity);
+		assertTrue(mButton != null);
+	}
+	
+	public void testButtonUI() {
+		
+		mActivity.runOnUiThread(
+	            new Runnable() {
+	                public void run() {
+	                    mButton.setText("Title!!");
+	                    String buttonText = (String) mButton.getText();
+	            		assertTrue( buttonText.equals("Title!!"));
+	                }
+	            }
+	        );
+	}
+	
+	public void testStateDestroy() {
+		mActivity.finish();
+		mActivity = this.getActivity();
+		assertNotNull(mActivity);
+	}
+	
+	public void testStatePause() {
+		Instrumentation instr = this.getInstrumentation();
+		assertNotNull(instr);
+		
+		instr.callActivityOnPause(mActivity);
+		//mButton.setText("NewTitle");
+		instr.callActivityOnResume(mActivity);
+		String buttonText = (String) mButton.getText();
+		assertTrue( buttonText.equals("Events") );
 	}
 }
