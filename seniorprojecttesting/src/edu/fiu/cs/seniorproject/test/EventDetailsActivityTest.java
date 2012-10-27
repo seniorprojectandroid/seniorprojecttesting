7adc942b2c863e59b36cc21753c424d22b72943a
Reinaldo <rhern071@fiu.edu>
2012-10-27 13:15:31 -0400

    datails activity

---
+++ seniorprojecttesting/src/edu/fiu/cs/seniorproject/test/EventDetailsActivityTest.java
@@ -0,0 +1,125 @@
+package edu.fiu.cs.seniorproject.test;
+
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+import edu.fiu.cs.seniorproject.EventDetailsActivity;
+import edu.fiu.cs.seniorproject.data.DateFilter;
+import edu.fiu.cs.seniorproject.data.Event;
+import edu.fiu.cs.seniorproject.data.EventCategoryFilter;
+import edu.fiu.cs.seniorproject.data.Location;
+import edu.fiu.cs.seniorproject.manager.AppLocationManager;
+import edu.fiu.cs.seniorproject.manager.DataManager;
+
+import android.app.Instrumentation;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class EventDetailsActivityTest extends ActivityInstrumentationTestCase2<EventDetailsActivity>{
+
+	private EventDetailsActivity mEventActivity = null;
+	private DataManager mDataManager = null;
+	
+	public EventDetailsActivityTest() {
+		super(EventDetailsActivity.class);
+	}
+	
+	@Override
+	protected void setUp() {
+		setActivityInitialTouchMode(false);
+		mEventActivity = getActivity();
+		mDataManager = DataManager.getSingleton();
+	}
+	
+	public void testPreconditions() {
+		assertNotNull(mEventActivity);
+		assertNotNull(mDataManager);
+	}
+	
+	@UiThreadTest
+	public void testActivityUI() {
+		ImageView img = (ImageView)mEventActivity.findViewById(edu.fiu.cs.seniorproject.R.id.image);
+		assertNotNull(img);
+				
+		TextView tvn = (TextView)mEventActivity.findViewById(edu.fiu.cs.seniorproject.R.id.event_name);
+		assertNotNull(tvn);
+		
+		TextView tvp = (TextView)mEventActivity.findViewById(edu.fiu.cs.seniorproject.R.id.event_place);
+		assertNotNull(tvp);
+		
+		TextView tvt = (TextView)mEventActivity.findViewById(edu.fiu.cs.seniorproject.R.id.event_time);
+		assertNotNull(tvt);
+		
+		TextView tvd = (TextView)mEventActivity.findViewById(edu.fiu.cs.seniorproject.R.id.event_distance);
+		assertNotNull(tvd);
+		
+		TextView tvds = (TextView)mEventActivity.findViewById(edu.fiu.cs.seniorproject.R.id.event_description);
+		assertNotNull(tvds);
+		
+		
+		View vw = (View)mEventActivity.findViewById(edu.fiu.cs.seniorproject.R.id.mapview);
+		assertNotNull(vw);
+		//assertTrue(pb.getVisibility() == View.VISIBLE);
+		
+		
+		// pedirle al data manader una lista y sacarle el primer event luego pasarcelo al showevent		
+		// mEventActivity.showEvent(event)
+		
+	}
+	
+	public void testDataProvider() {
+		assertNotNull(mDataManager);
+		
+		android.location.Location currentLocation = AppLocationManager.getCurrentLocation();
+		Location location = new Location( String.valueOf( currentLocation.getLatitude() ), String.valueOf(currentLocation.getLongitude()) );
+		
+		final List<Event> eventList = mDataManager.getEventList(location, EventCategoryFilter.Music, "10", null,DateFilter.THIS_WEEK);
+		assertNotNull(eventList);
+		assertTrue(eventList.size() > 0);
+		
+		final Event event = eventList.get(0);
+		
+		final CountDownLatch latch = new CountDownLatch(1);
+		
+		mEventActivity.runOnUiThread(new Runnable() {
+			
+			@Override
+			public void run() {
+				mEventActivity.showEvent(event);
+				latch.countDown();
+			}
+		});
+       
+       	try {
+			latch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void testStateDestroy() {
+		mEventActivity.finish();
+		mEventActivity = this.getActivity();
+		assertNotNull(mEventActivity);
+	}
+	
+	@UiThreadTest
+	public void testStatePause() {
+		Instrumentation instr = this.getInstrumentation();
+		assertNotNull(instr);
+		
+		instr.callActivityOnPause(mEventActivity);
+		instr.callActivityOnResume(mEventActivity);
+	}
+	
+	
+	
+	
+}// end EventDetailsActivity
+
---
+++ seniorprojecttesting/src/edu/fiu/cs/seniorproject/test/PlaceDetailsActivityTest.java
@@ -0,0 +1,112 @@
+package edu.fiu.cs.seniorproject.test;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+import edu.fiu.cs.seniorproject.PlaceDetailsActivity;
+import edu.fiu.cs.seniorproject.data.Location;
+import edu.fiu.cs.seniorproject.data.Place;
+import edu.fiu.cs.seniorproject.data.PlaceCategoryFilter;
+import edu.fiu.cs.seniorproject.manager.AppLocationManager;
+import edu.fiu.cs.seniorproject.manager.DataManager;
+import android.app.Instrumentation;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class PlaceDetailsActivityTest extends ActivityInstrumentationTestCase2<PlaceDetailsActivity> {
+	
+	private PlaceDetailsActivity mPlaceActivity = null;
+	private DataManager mDataManager = null;
+
+	public PlaceDetailsActivityTest() {
+		super(PlaceDetailsActivity.class);
+		// TODO Auto-generated constructor stub
+	}// end PlaceDetailsActivityTest	
+	
+	@Override
+	protected void setUp() {
+		setActivityInitialTouchMode(false);
+		mPlaceActivity = getActivity();
+		mDataManager = DataManager.getSingleton();
+	}// end setUp
+	
+	public void testPreconditions() {
+		assertNotNull(mPlaceActivity);
+		assertNotNull(mDataManager);
+	}// testPreconditions
+	
+	@UiThreadTest
+	public void testActivityUI() {
+		
+		ImageView img = (ImageView)mPlaceActivity.findViewById(edu.fiu.cs.seniorproject.R.id.place_image);
+		assertNotNull(img);
+				
+		TextView tvn = (TextView)mPlaceActivity.findViewById(edu.fiu.cs.seniorproject.R.id.place_name);
+		assertNotNull(tvn);
+		
+		TextView tvloc = (TextView)mPlaceActivity.findViewById(edu.fiu.cs.seniorproject.R.id.place_location);
+		assertNotNull(tvloc);
+		
+		TextView tvdist = (TextView)mPlaceActivity.findViewById(edu.fiu.cs.seniorproject.R.id.place_distance);
+		assertNotNull(tvdist);
+		
+		TextView tvdesc = (TextView)mPlaceActivity.findViewById(edu.fiu.cs.seniorproject.R.id.place_description);
+		assertNotNull(tvdesc);		
+		
+		TextView tvevent = (TextView)mPlaceActivity.findViewById(android.R.id.empty);// see with julio
+		assertNotNull(tvevent);
+		
+		View vw = (View)mPlaceActivity.findViewById(edu.fiu.cs.seniorproject.R.id.mapview);
+		assertNotNull(vw);		
+		
+	}// end testActivityUI
+	
+	public void testDataProvider() {
+		assertNotNull(mDataManager);
+		
+		android.location.Location currentLocation = AppLocationManager.getCurrentLocation();
+		Location location = new Location( String.valueOf( currentLocation.getLatitude() ), String.valueOf(currentLocation.getLongitude()) );
+		
+		final List<Place> eventList = mDataManager.getPlaceList(location, PlaceCategoryFilter.HOTEL, "8","");// see parameters
+		assertNotNull(eventList);
+		assertTrue(eventList.size() > 0);
+		
+		final Place place = eventList.get(0);
+		
+		final CountDownLatch latch = new CountDownLatch(1);
+		
+		mPlaceActivity.runOnUiThread(new Runnable() {
+			
+			@Override
+			public void run() {
+				mPlaceActivity.showPlace(place);
+				latch.countDown();
+			}
+		});
+       
+       	try {
+			latch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}		
+		
+	}// end testDataProvider
+	
+	public void testStateDestroy() {
+		mPlaceActivity.finish();
+		mPlaceActivity = this.getActivity();
+		assertNotNull(mPlaceActivity);
+	}// end testStateDestroy
+	
+	@UiThreadTest
+	public void testStatePause() {
+		Instrumentation instr = this.getInstrumentation();
+		assertNotNull(instr);
+		
+		instr.callActivityOnPause(mPlaceActivity);
+		instr.callActivityOnResume(mPlaceActivity);
+	}// end testStatePause	
+
+}// end PlaceDetailsActivityTest
